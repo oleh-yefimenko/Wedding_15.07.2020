001a9de98647f4cb34fd50eb27de5b9459f1b016
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2020-07-17 14:04:50 +0200

    сверстал services

---
+++ js/services__slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	$('.services__slider').slick({
+		slidesToShow: 3,
+		arrows: true,
+		appendArrows: $('.slider__arrows'),
+		// autoplay: true,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		responsive: [
+			{
+				breakpoint: 815,
+				settings: {
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 555,
+				settings: {
+					slidesToShow: 1,
+					fade: true,
+					// appendArrows: $('.slider'), //----> для перемешения стрелок по бокам
+				}
+			},
+		],
+	});
+});
